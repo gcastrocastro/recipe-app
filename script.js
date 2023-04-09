bb88b7bff8d43a0a8df4f3efd6e7beee374ecd2f
gcastrocastro <gcastrocastro.25@gmail.com>
2023-04-09 13:13:08 -0400

    fix: spacing, dlt cmmts

--- script.js
+++ script.js
@@ -9,16 +9,10 @@ const mealInfoEl = document.getElementById("meal-info");
 getRandomMeal();
 fetchFaveMeals();
 
-// async is to allow the fetching of the API/ not really sure...
-// the method fetch returns a promise object from an asynchronous operation
-// then, the json method parses the promise object into a js object so we can read it.
-// the information we need is in the 1st object[0] of the array[meals] we receive
 async function getRandomMeal(){
-    const resp = await fetch
-        ('https://www.themealdb.com/api/json/v1/1/random.php');
+    const resp = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
     const respData = await resp.json();
-    const randomMeal = respData.meals[0]
-
+    const randomMeal = respData.meals[0];
     addMeal(randomMeal, true);
 }
 
@@ -36,14 +30,9 @@ async function getMealBySearch(term){
     return meals;
 }
 
-// this addMeal function is in conjunction with the getRandomMeal function above
-// it takes in randomMeal and assigns it a false variable if it is random
 function addMeal(mealData, random = false) {
     const meal = document.createElement('div');
-
     meal.classList.add('meal');
-
-    // the $jquery if/or is saying if we have a random-value/false-value then we add the span, if not, an empty string
     meal.innerHTML = (`
             <div class="meal-header">
                 ${random ? `<span class="random"> Random Recipe </span>` : ''}
@@ -57,7 +46,6 @@ function addMeal(mealData, random = false) {
             </div>`);
 
     const btn = meal.querySelector('.meal-body .fave-btn'); 
-           
     btn.addEventListener("click", () => {
         if (btn.classList.contains('active')) {
             removeMealfromLocalStorage(mealData.idMeal);
@@ -66,22 +54,17 @@ function addMeal(mealData, random = false) {
             addMealtoLocalStorage(mealData.idMeal);
             btn.classList.add('active');
         }
-        // this is to fetch the updated data every time we click the fave btn
         fetchFaveMeals();
     });
 
     meal.addEventListener('click', () => {
         updateMealInfo(mealData);
     });
-
     mealsEl.appendChild(meal);
 }
 
 function updateMealInfo(mealData){
-    // first we clear the container 
     mealInfoEl.innerHTML = '';
-
-    // Get ingredients and measurements
     const ingredients = [];
 
     for(let i=1; i<=20; i++) {
@@ -92,9 +75,7 @@ function updateMealInfo(mealData){
         }
     }
 
-    // update the meal info
     const mealEl = document.createElement('div');
-
     mealEl.innerHTML = `
             <h1>${mealData.strMeal}</h1>
             <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
@@ -105,14 +86,9 @@ function updateMealInfo(mealData){
                     `<li>${ing}</li>`)
                     .join('')}
             </ul>`
-
     mealInfoEl.appendChild(mealEl);
-
-    // this shows the popup
     mealPopup.classList.remove('hidden');
 }
-
-// Adding/Removing from Local Storage
 
 function addMealtoLocalStorage(mealId) {
     const mealIds = getMealsFromLocalStorage();
@@ -126,69 +102,48 @@ function removeMealfromLocalStorage(mealId) {
 
 function getMealsFromLocalStorage() {
     const mealIds = JSON.parse(localStorage.getItem('mealIds'));
-
-    // what is this for?? 
     return mealIds === null ? [] : mealIds;
 }
 
 async function fetchFaveMeals(){
-    // this is to make the favorite container clear whenever we fetch a new meal
     favoriteContainer.innerHTML='';
-
     const mealIds = getMealsFromLocalStorage();
-
     for(let i=0; i<mealIds.length; i++){
         const mealId = mealIds[i];
         const meal = await getMealById(mealId);
-
         addMealtoFave(meal);
     }
 }
 
-// adding the fave meals to the screen list
-
 function addMealtoFave(mealData) {
     const faveMeal = document.createElement('li');
-    // const heart = document.querySelector('.fave-btn');
-
-    // the $jquery if/or is saying if we have a random-value/false-value 
-    // then we add the span, if not, an empty string
     faveMeal.innerHTML = (`
     <img src="${mealData.strMealThumb}" 
     alt="${mealData.strMeal}">
     <span>${mealData.strMeal}</span>
     <button class="clear"><i class="fa-solid fa-circle-xmark"></i></button>`);
 
-    // NOTE: this constant needs to be placed after the innerHTML bc it's called 
-    // AFTER the html has been added.
     const btn = faveMeal.querySelector('.clear');
-    
     btn.addEventListener('click', () => {
-            // heart.classList.toggle('active');
-            removeMealfromLocalStorage(mealData.idMeal);
-            fetchFaveMeals();
+        removeMealfromLocalStorage(mealData.idMeal);
+        fetchFaveMeals();
     });
-
     faveMeal.addEventListener('click', () => {
         updateMealInfo(mealData);
     });
-
     favoriteContainer.appendChild(faveMeal);
 }
 
 searchBtn.addEventListener('click', async () => {
-    // this clears the meal container first after every search
     mealsEl.innerHTML = '';
     const search = searchTerm.value;
     const meals = await getMealBySearch(search);
-
     if (meals) {
         meals.forEach((meal) => {
             addMeal(meal);
         });
     }
 });
-
 
 popupCloseBtn.addEventListener('click', () => {
     mealPopup.classList.add('hidden');
